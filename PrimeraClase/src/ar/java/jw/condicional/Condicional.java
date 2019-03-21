0190d4864f99f8345bdb8daea97a8baa4548d230
Axel <AxelBoltok@gmail.com>
2019-03-20 21:34:29 -0300

    primer comiteo

---
+++ PrimeraClase/src/ar/java/jw/condicional/Condicional.java
@@ -0,0 +1,19 @@
+package ar.java.jw.condicional;
+
+public class Condicional {
+	
+	public static void main(String[] args) {
+		
+		int valor = 0; 
+		
+		if(valor < 0) {
+			System.out.println("el valor es negativo");
+			
+		} else {
+			System.out.println(valor + " este es el valor");
+			
+		}
+		System.out.println("estoy afuera");
+	}
+
+}
---
+++ PrimeraClase/src/ar/java/jw/paquete/PrimerPrograma.java
@@ -0,0 +1,16 @@
+package ar.java.jw.paquete;
+
+public class PrimerPrograma {
+	
+	public static void main(String[] args) {
+			
+		//System.out.println("Hello Word");
+		
+		
+		
+		
+		
+		
+	}
+
+}
